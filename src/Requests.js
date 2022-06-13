103be367bd0f8924915ccdc740a7e3dda20eb2c3
Yash Sonawane <95970480+yxshgithub@users.noreply.github.com>
2022-06-14 02:27:07 +0530

    adding readme attachments

--- src/Requests.js
+++ src/Requests.js
@@ -8,4 +8,4 @@ const requests = {
     requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
   };
 
-  export default requests
+export default requests
--- src/firebase.js
+++ src/firebase.js
@@ -35,3 +35,4 @@ export const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
